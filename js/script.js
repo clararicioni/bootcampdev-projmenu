831a4c9a59f5eaef68563779dc3abd31fa70c605
Clara Ricioni <bergamoclara9@gmail.com>
2025-03-09 23:23:59 -0300

    closes #5 - feat: number format

--- js/script.js
+++ js/script.js
@@ -6,6 +6,11 @@ function calcQuantidade() {
     var nome = document.getElementById("name").value;
     var output = document.getElementById("output");
 
+    var formatter = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+    });
+
     output.innerHTML = `
         <h2>Caro(a) ${nome},</h2>
         <p>Seguem os dados do seu pedido.</p>
@@ -16,10 +21,10 @@ function calcQuantidade() {
         if (input.value > 0) {
             var price = document.getElementById(input.id).getAttribute("data-price");
             var total = price * input.value;
-            output.innerHTML += `Prato: ${input.id} - Preço unitário: R$ ${price} - Quantidade: ${input.value} - Total: R$ ${total}</br>`;
+            output.innerHTML += `Prato: ${input.id} - Preço unitário: ${formatter.format(price)} - Quantidade: ${input.value} - Total: ${formatter.format(total)}</br>`;
             finalPrice += price * input.value;
         }
     }
-    output.innerHTML += `<h2>Preço final: R$ ${finalPrice.toFixed(2)}</h2>`;
+    output.innerHTML += `<h2>Preço final: ${formatter.format(finalPrice)}</h2>`;
 }
 
